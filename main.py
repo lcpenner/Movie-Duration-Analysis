b1b718bc7b89b622bce3229b89bc9f06fcbabc08
Luke <95511211+lcpenner@users.noreply.github.com>
2022-09-14 10:02:15 -0700

    Update to add more comments

--- main.py
+++ main.py
@@ -13,31 +13,22 @@ netflix_df_movies_only = netflix_df[netflix_df["type"] == "Movie"]
 # Select only the columns of interest
 netflix_movies_col_subset = netflix_df_movies_only[["title", "country", "genre", "release_year", "duration"]]
 
-# Print the first five rows of the new DataFrame
-print(netflix_movies_col_subset[0:5])
-
 # Create a figure and increase the figure size
 fig = plt.figure(figsize=(12,8))
 
 # Create a scatter plot of duration versus year
 plt.scatter(netflix_movies_col_subset['release_year'],netflix_movies_col_subset['duration'] )
 
-# Create a title
 plt.title('Movie Duration by Year of Release')
 
-# Show the plot
 plt.show()
 
+# Since most shorter movies are in the genre Children, Documentaries, or Stand-up, create a second plot showing these movies in different colors.
 # Filter for durations shorter than 60 minutes
 short_movies = netflix_movies_col_subset[netflix_movies_col_subset['duration'] < 60]
 
-# Print the first 20 rows of short_movies
-print(short_movies[0:20])
-
-# Define an empty list
 colors = []
 
-# Iterate over rows of netflix_movies_col_subset
 for lab, row in netflix_movies_col_subset.iterrows():
     if row['genre'] == 'Children':
         colors.append('red')
@@ -48,20 +39,13 @@ for lab, row in netflix_movies_col_subset.iterrows():
     else:
         colors.append('black')
 
-# Inspect the first 10 values in your list
-print(colors[0:10])
-
 # Set the figure style and initalize a new figure
 plt.style.use('fivethirtyeight')
 fig = plt.figure(figsize=(12,8))
 
 # Create a scatter plot of duration versus release_year
 plt.scatter(netflix_movies_col_subset['release_year'],netflix_movies_col_subset['duration'], c = colors )
-
-# Create a title and axis labels
 plt.title("Movie duration by year of release")
 plt.xlabel("Release year")
 plt.ylabel("Duration (min)")
-
-# Show the plot
 plt.show()
